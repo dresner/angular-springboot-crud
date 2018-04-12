295dad2faec0ad40731bf7e521bc28c10ef8ba02
Davi Resner <daviresner@gmail.com>
2018-04-12 13:00:25 -0300

    Tab fix

--- spring-server/src/main/java/Controller.java
+++ spring-server/src/main/java/Controller.java
@@ -12,32 +12,32 @@ public class Controller {
         this.db = new Database();
     }
 
-	@RequestMapping("/")
-	String home() {
+    @RequestMapping("/")
+    String home() {
         return "Hello!";
-	}
+    }
 
-	@RequestMapping(value = "/get", method = RequestMethod.GET)
-	Collection<Record> get(@RequestParam("name") Optional<String> name, 
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    Collection<Record> get(@RequestParam("name") Optional<String> name, 
             @RequestParam("artist") Optional<String> artist, 
             @RequestParam("year") Optional<Integer> year) {
        return this.db.getRecords(name, artist, year); 
-	}
+    }
 
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     boolean add(@RequestBody Record record) {
         return db.add(record);
     }
 
     // TODO: DELETE shouldn't have a body
-	@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
     boolean remove(@RequestBody Record record) {
         return db.remove(record);
     }
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Controller.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Controller.class, args);
+    }
 
     private Database db;
 }
